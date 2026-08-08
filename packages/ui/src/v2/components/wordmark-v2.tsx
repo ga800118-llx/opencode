46257c5dcf86f19466be7fc3e27fc49360e6b1c9
kdtc <kdtc@kdtcdeMacBook-Pro-2.local>
2026-08-08 19:50:38 +0800

    fix(app): replace new task background

--- packages/ui/src/v2/components/wordmark-v2.tsx
+++ packages/ui/src/v2/components/wordmark-v2.tsx
@@ -11,51 +11,22 @@ export function WordmarkV2(props: Pick<ComponentProps<"svg">, "class">) {
       fill="none"
       classList={{ [props.class ?? ""]: !!props.class }}
     >
-      <g opacity="0.6">
-        <g mask={`url(#${mask})`}>
-          <g opacity="0.16">
-            <path
-              opacity="0.7"
-              d="M55.3846 36.4286H18.4615V91.7143H55.3846V36.4286ZM73.8462 110.143H0V18H73.8462V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M110.462 91.7143H147.385V36.4286H110.462V91.7143ZM165.846 110.143H110.462V128.571H92V18H165.846V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M258.846 73.2857H203.462V91.7143H258.846V110.143H185V18H258.846V73.2857ZM203.462 54.8571H240.385V36.4286H203.462V54.8571Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M332.385 36.4286H295.462V110.143H277V18H332.385V36.4286ZM350.846 110.143H332.385V36.4286H350.846V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M442.846 36.4286H387.462V91.7143H442.846V110.143H369V18H442.846V36.4286Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M517.385 36.4286H480.462V91.7143H517.385V36.4286ZM535.846 110.143H462V18H535.846V110.143Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M609.385 36.8571H572.462V92.1429H609.385V36.8571ZM627.846 110.571H554V18.4286H609.385V0H627.846V110.571Z"
-              fill="currentColor"
-            />
-            <path
-              opacity="0.7"
-              d="M664.462 36.4286V54.8571H701.385V36.4286H664.462ZM719.846 73.2857H664.462V91.7143H719.846V110.143H646V18H719.846V73.2857Z"
-              fill="currentColor"
-            />
-          </g>
-        </g>
+      <g opacity="0.6" mask={`url(#${mask})`}>
+        <path d="M0 18H92V41H0V18Z" fill="currentColor" />
+        <path d="M0 18H23V110H0V18Z" fill="currentColor" />
+        <path d="M0 87H92V110H0V87Z" fill="currentColor" />
+        <path d="M46 55H92V78H46V55Z" fill="currentColor" />
+        <path d="M69 78H92V110H69V78Z" fill="currentColor" opacity="0.65" />
+        <text
+          x="122"
+          y="93"
+          fill="currentColor"
+          font-family="IBM Plex Sans, Arial, sans-serif"
+          font-size="76"
+          font-weight="700"
+        >
+          Guai Code
+        </text>
       </g>
       <defs>
         <mask id={mask} style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="720" height="129">
